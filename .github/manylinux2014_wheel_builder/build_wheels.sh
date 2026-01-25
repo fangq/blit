b4be4407f45ceae271d6c9a02de4a92a0ee9b587
Qianqian Fang <fangqq@gmail.com>
2026-01-24 21:29:09 -0500

    [ci] adjust setuptools version

--- .github/manylinux2014_wheel_builder/build_wheels.sh
+++ .github/manylinux2014_wheel_builder/build_wheels.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd python/
-for PYBIN in /opt/python/cp3{7,8,9,10,11,12}*/bin/; do
+for PYBIN in /opt/python/cp3{8,9,10,11,12}*/bin/; do
     if [ -d "$PYBIN" ]; then
-        "${PYBIN}/pip" install wheel setuptools "numpy<2.0"
+        "${PYBIN}/pip" install wheel "setuptools<65.0" "numpy>=1.25,<2.0"
         "${PYBIN}/python" setup.py bdist_wheel -d wheels/
     fi
 done
