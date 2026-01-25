2c504e9c02df2b899479f151d838ce5b9de489a4
Qianqian Fang <fangqq@gmail.com>
2026-01-24 19:43:09 -0500

    [ci] ensures numpy.distutils is available during the build process.

--- .github/manylinux2014_wheel_builder/build_wheels.sh
+++ .github/manylinux2014_wheel_builder/build_wheels.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd python/
 for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install "numpy<2.0"
     "${PYBIN}/pip" wheel . -w wheels/
 done
 rm -rf dist/
@@ -8,4 +9,3 @@ for WHEEL in wheels/*; do
     auditwheel repair ${WHEEL} -w dist/
 done
 rm -rf wheels/
-
