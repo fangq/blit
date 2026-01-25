1aeea7814f2e4c5f5182d39364cb76f9bca1633d
Qianqian Fang <fangqq@gmail.com>
2026-01-24 19:34:46 -0500

    [ci] add github action for building and deploying blit

---
+++ .github/check-pypi-upload.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+BLIT_BUILD_VERSION=$(awk -F"-" '{ print $2 }' <<< $(ls dist/ | head -1))
+BLIT_VERSIONS_STRING=$(pip index versions blit | grep versions:)
+BLIT_VERSIONS_STRING=${BLIT_VERSIONS_STRING#*:}
+UPLOAD_TO_PYPI=1
+while IFS=', ' read -ra BLIT_VERSIONS_ARRAY; do
+  for VERSION in "${BLIT_VERSIONS_ARRAY[@]}"; do
+    if [ "$BLIT_BUILD_VERSION" = "$VERSION" ]; then
+      UPLOAD_TO_PYPI=0
+    fi
+  done;
+done <<< "$BLIT_VERSIONS_STRING"
+if [ "$UPLOAD_TO_PYPI" = 1 ]; then
+  echo "Wheel version wasn't found on PyPi.";
+else
+  echo "Wheel was found on PyPi.";
+fi
+echo "perform_pypi_upload=$UPLOAD_TO_PYPI" >> $GITHUB_OUTPUT
+
---
+++ .github/manylinux2014_wheel_builder/build_wheels.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd python/
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" wheel . -w wheels/
+done
+rm -rf dist/
+for WHEEL in wheels/*; do
+    auditwheel repair ${WHEEL} -w dist/
+done
+rm -rf wheels/
+
