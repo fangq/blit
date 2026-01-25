766a5dd484f7a099d06d883a90531c491972c415
Qianqian Fang <fangqq@gmail.com>
2026-01-24 19:53:32 -0500

    [ci] fix build errors

--- .github/manylinux2014_wheel_builder/build_wheels.sh
+++ .github/manylinux2014_wheel_builder/build_wheels.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 cd python/
-for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install "numpy<2.0"
-    "${PYBIN}/pip" wheel . -w wheels/
+for PYBIN in /opt/python/cp3{7,8,9,10,11,12}*/bin/; do
+    if [ -d "$PYBIN" ]; then
+        "${PYBIN}/pip" install wheel setuptools "numpy<2.0"
+        "${PYBIN}/python" setup.py bdist_wheel -d wheels/
+    fi
 done
 rm -rf dist/
 for WHEEL in wheels/*; do
     auditwheel repair ${WHEEL} -w dist/
 done
-rm -rf wheels/
+rm -rf wheels/ build/ *.egg-info/
